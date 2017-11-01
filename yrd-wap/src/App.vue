ba3bc3b5d426ee46b82f81f88ce2ab24c32a23ad
subBoy <15256025969@163.com>
2017-11-01 10:25:19 +0800

    路由变化时监听网络连接状况

--- yrd-wap/src/App.vue
+++ yrd-wap/src/App.vue
@@ -5,7 +5,27 @@
 </template>
 
 <script>
-export default {}
+import axios from 'axios'
+
+axios.defaults.withCredentials = true
+
+export default {
+  created () {
+    this.$router.beforeEach((to, from, next) => {
+      axios.get('/static/inter.json', {}).then((res) => {
+        if (res.status === 200) {
+          next()
+        } else {
+          return
+        }
+      }).catch((err) => {
+        if (err) {
+          location.href = 'www.yourendai.com'
+        }
+      })
+    })
+  }
+}
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
